deaf7640099e159bd91c5efe117a7a52836b04b6
ThatFnafguy <85533879+ThatFnafguy@users.noreply.github.com>
2022-05-05 18:22:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+song1="";
+
+song2="";
+
+song1_status="";
+song2_status="";
+socreRightWrist=0;
+socreRightWrist=0;
+socreLeftWrist=0;
+leftWristX = 0;
+leftWristy = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+song1=loadSound("music.mp3");
+song2=loadSound("music2.mp3");
+}
+
+function setup(){
+canvas=createCanvas(600,500);
+canvas.position(650,250);
+
+video = createCapture(VIDEO);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+image(video,0,0,600,500);
+song1_status=song1.isPlaying();
+song2_status=song2.isPlaying();
+fill("#FF0000");
+stroke("#FF0000");
+if(scoreRightWrist > 0.2){
+circle(rightWristX,rightWristY,20);
+song2.stop();
+if(song1_status==false){
+song1.play();
+document.getElementById("song").innerHTML="playinghairypotter";
+}
+}
+}
+if(scorerightwrist > 0.2){
+    circle(leftWristX,leftWristY,20);
+    song1.stop();
+    if(song2_status==false){
+    song1.play();
+    document.getElementById("song").innerHTML="zelda";
+    }
+
+function play(){
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
+
+function modelLoaded(){
+    console.log('poseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        socreLeftWrist=results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist="+ socreRightWrist + "scoreLeftWrist="+ socreLeftWrist);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX +" leftWristY ="+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX +" righttWristY ="+ rightWristY);
+    }
+}
+}
